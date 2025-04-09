27186e0bab19969d72b9f89fb74a8801e5e0aac0
Bernardo Belchior <bernardo@mui.com>
2025-04-09 17:38:56 +0100

    Wait for animation frame before printing

--- packages/x-charts-pro/src/internals/plugins/useChartProExport/useChartProExport.ts
+++ packages/x-charts-pro/src/internals/plugins/useChartProExport/useChartProExport.ts
@@ -2,16 +2,32 @@ import { ChartPlugin } from '@mui/x-charts/internals';
 import { printChart } from './print';
 import { ChartPrintExportOptions, UseChartProExportSignature } from './useChartProExport.types';
 
+function waitForAnimationFrame() {
+  let resolve;
+
+  const promise = new Promise((res) => {
+    resolve = res;
+  });
+
+  window.requestAnimationFrame(() => {
+    resolve();
+  });
+
+  return promise;
+}
+
 export const useChartProExport: ChartPlugin<UseChartProExportSignature> = ({
   chartRootRef,
   instance,
 }) => {
-  const print = (options?: ChartPrintExportOptions) => {
+  const print = async (options?: ChartPrintExportOptions) => {
     const chartRoot = chartRootRef.current;
 
     if (chartRoot) {
       const enableAnimation = instance.disableAnimation();
       try {
+        // Wait for animation frame to ensure the animation finished
+        await waitForAnimationFrame();
         printChart(chartRoot, options);
       } finally {
         enableAnimation();
