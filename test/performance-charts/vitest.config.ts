c93bb6b1132d076e38e96bceaf49ff1b24ba84e5
Bernardo Belchior <bernardo@mui.com>
2025-07-01 09:31:11 +0200

    Use custom reporter

--- test/performance-charts/vitest.config.ts
+++ test/performance-charts/vitest.config.ts
@@ -1,12 +1,51 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import { Reporter } from 'vitest/reporters';
+import { RunnerTask } from 'vitest';
 
 const isCI = process.env.CI === 'true';
 const isTrace = !isCI && process.env.TRACE === 'true';
 
+const ciReporter: Reporter = {
+  onTestSuiteResult: (testSuite) => {
+    const benchmarks: RunnerTask[] = testSuite.task.tasks.filter((t) => t.meta.benchmark);
+    const benchmarkResults = benchmarks
+      .map((b) => b.result?.benchmark)
+      .filter((b) => b !== undefined);
+
+    const table = benchmarkResults.map((result) => ({
+      name: result.name,
+      mean: result.mean,
+      median: result.median,
+      p99: result.p99,
+      standardDeviation: result.sd,
+      marginOfError: result.moe,
+      sampleCount: result.sampleCount,
+    }));
+
+    const tableWoIndex = table.reduce((acc, row) => {
+      acc[row.name] = row;
+      return acc;
+    }, {});
+
+    // console.log(table);
+    console.table(tableWoIndex, [
+      'mean',
+      'median',
+      'p99',
+      'standardDeviation',
+      'marginOfError',
+      'sampleCount',
+    ]);
+  },
+};
+
 export default defineConfig({
   plugins: [react()],
   test: {
+    benchmark: {
+      reporters: ['default', ciReporter],
+    },
     setupFiles: ['./setup.ts'],
     env: { TRACE: isTrace ? 'true' : 'false' },
     environment: isTrace ? 'node' : 'jsdom',
