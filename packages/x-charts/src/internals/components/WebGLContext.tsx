303fd86b07ad26502b039cca181182d2b0212baa
Bernardo Belchior <bernardo@mui.com>
2026-01-15 09:59:55 +0100

    Fix lint

--- packages/x-charts/src/internals/components/WebGLContext.tsx
+++ packages/x-charts/src/internals/components/WebGLContext.tsx
@@ -24,7 +24,7 @@ export const WebGLProvider = React.forwardRef<
   const handleRef = useForkRef(canvasRef, ref);
   const chartRoot = useChartRootRef().current;
   const drawingArea = useDrawingArea();
-  const [_, rerender] = React.useReducer((s) => s + 1, 0);
+  const [, rerender] = React.useReducer((s) => s + 1, 0);
 
   // FIXME: Why isn't chart root available on the first render?
   React.useEffect(() => {
